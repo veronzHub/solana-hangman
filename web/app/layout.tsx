94f29416f9ab1ec00a3128841d606d5df9cbd9c0
veronzHub <veronica.coulombe@gmail.com>
2024-07-18 15:46:17 +0000

    fixed theme issue

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body>
         <ReactQueryProvider>
           <ClusterProvider>
