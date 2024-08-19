0b5e113fc49086d0e0d2179bf5a897ebddfc4a3e
veronzHub <veronica.coulombe@gmail.com>
2024-08-19 14:25:50 +0000

    updated page titles

--- web/app/hangman/play/[address]/page.tsx
+++ web/app/hangman/play/[address]/page.tsx
@@ -1,5 +1,10 @@
 import HangmanAddressPage from '@/components/hangman/hangman-play-address';
 
+export const metadata = {
+  title: 'Play Hangman',
+  description: 'Play Hangman',
+};
+
 export default function Page() {
   return <HangmanAddressPage />;
 }
